4ed6564c27663284ac744fe3dac4e9ac9b86334f
nguyenkhoa0209 <liverpool292000@gmail.com>
2023-09-26 16:46:10 +0200

    fix 1

--- docs_source/source/conf.py
+++ docs_source/source/conf.py
@@ -30,8 +30,12 @@ release = '0.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.mathjax', 'nbsphinx'] #
+
+# napoleon_use_rtype = False
+
+# generate autosummary even if no references
+# autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -41,14 +45,19 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
-# -- Options for HTML output -------------------------------------------------
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_sidebars = {'**': ['localtoc.html', 'sourcelink.html', 'searchbox.html']}
 
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False
+}
+html_title = 'tpinns'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
